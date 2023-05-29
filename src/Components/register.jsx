5e249ead24a753b13abdff16e9f730c69e213d55
Jaloliddin <jaloliddinabdurahmmonov13@gmail.com>
2023-05-29 15:27:08 +0500

    first commit

--- src/Components/register.jsx
+++ src/Components/register.jsx
@@ -8,7 +8,7 @@ const Register = () => {
       <h1 className="h3 mb-3 fw-normal mt-3">Please register</h1>
       <div className="form-floating">
         <input type="text" className="form-control" id="floatingInput" placeholder="username" />
-        <label for="floatingInput">UserName</label>
+        <label for="floatingInput">Username</label>
       </div>
       <div className="form-floating">
         <input type="email" className="form-control my-2" id="floatingInput" placeholder="name@example.com" />
