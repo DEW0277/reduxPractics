6b09b91e1d83a6961cf7865d063c4a3acd73713a
Jaloliddin <jaloliddinabdurahmmonov13@gmail.com>
2023-05-30 19:42:43 +0500

    added login code

--- src/Components/login.jsx
+++ src/Components/login.jsx
@@ -1,9 +1,78 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Login = () => {
   return (
-    <div>Login</div>
-  )
-}
+    <div className="container">
+      <h3 className="text-center">Please Login</h3>
+      <form className="w-25 my-3 m-auto">
+        <div className="form-outline mb-4">
+          <input type="email" id="form2Example1" className="form-control" />
+          <label className="form-label" for="form2Example1">
+            Email address
+          </label>
+        </div>
 
-export default Login
+        <div className="form-outline mb-4">
+          <input type="password" id="form2Example2" className="form-control" />
+          <label className="form-label" for="form2Example2">
+            Password
+          </label>
+        </div>
+
+        <div className="row mb-4">
+          <div className="col d-flex justify-content-center">
+            <div className="form-check">
+              <input
+                className="form-check-input"
+                type="checkbox"
+                value=""
+                id="form2Example31"
+                checked
+              />
+              <label className="form-check-label" for="form2Example31">
+                {" "}
+                Remember me{" "}
+              </label>
+            </div>
+          </div>
+
+          <div className="col">
+            <a href="#!">Forgot password?</a>
+          </div>
+        </div>
+
+        <button
+          type="button"
+          className="btn text-center btn-primary btn-block mb-4"
+        >
+          Sign in
+        </button>
+
+        <div className="text-center">
+          <p>
+            Not a member? <Link to={"/register"}>Register</Link>
+          </p>
+          <p>or sign up with:</p>
+          <button type="button" className="btn btn-link btn-floating mx-1">
+            <i className="fab fa-facebook-f"></i>
+          </button>
+
+          <button type="button" className="btn btn-link btn-floating mx-1">
+            <i className="fab fa-google"></i>
+          </button>
+
+          <button type="button" className="btn btn-link btn-floating mx-1">
+            <i className="fab fa-twitter"></i>
+          </button>
+
+          <button type="button" className="btn btn-link btn-floating mx-1">
+            <i className="fab fa-github"></i>
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
