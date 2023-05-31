496bd3702a9713694c6f14e294125c31f341efae
Jaloliddin <jaloliddinabdurahmmonov13@gmail.com>
2023-05-31 13:49:33 +0500

    added new  login code

--- src/Components/login.jsx
+++ src/Components/login.jsx
@@ -1,28 +1,43 @@
 import React, { useState } from "react";
 import Input from "../ui/input";
+import { useDispatch, useSelector } from "react-redux";
+import { loginUserStart } from "../slice/auth";
 
 const Login = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const dispatch = useDispatch();
+  const { isLoading } = useSelector((state) => state.auth);
+
+  const loginHandler = (e) => {
+    e.preventDefault();
+    dispatch(loginUserStart());
+  };
+
   return (
     <div className="text-center">
-    <main className="form-signin w-25 m-auto">
-      <form>
-        <h1 className="h3 mb-3 fw-normal mt-3">Please login</h1>
-        <Input label={"Email address"} state={email} setState={setEmail} />
-        <Input
-          label={"Password"}
-          type={"password"}
-          state={password}
-          setState={setPassword}
-        />
-        <button className="w-100 btn btn-lg btn-primary mt-3" type="submit">
-          Login
-        </button>
-        <p className="mt-5 mb-3 text-body-secondary">© 2017–2023</p>
-      </form>
-    </main>
-  </div>
+      <main className="form-signin w-25 m-auto">
+        <form>
+          <h1 className="h3 mb-3 fw-normal mt-3">Please login</h1>
+          <Input label={"Email address"} state={email} setState={setEmail} />
+          <Input
+            label={"Password"}
+            type={"password"}
+            state={password}
+            setState={setPassword}
+          />
+          <button
+            className="w-100 btn btn-lg btn-primary mt-3"
+            disabled={isLoading}
+            onClick={loginHandler}
+            type="submit"
+          >
+            {isLoading ? "Loading..." : "Login"}
+          </button>
+          <p className="mt-5 mb-3 text-body-secondary">© 2017–2023</p>
+        </form>
+      </main>
+    </div>
   );
 };
 
